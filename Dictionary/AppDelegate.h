acc29c6cb17c1e935dff94b7f7d11126fe7deb88
IM Hoang Thai <hoangthai95ne@gmail.com>
2015-12-17 10:45:02 +0700

    Initial Commit

---
+++ Dictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dictionary
+//
+//  Created by Hoàng Thái on 12/17/15.
+//  Copyright © 2015 HOANGTHAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dictionary
+//
+//  Created by Hoàng Thái on 12/17/15.
+//  Copyright © 2015 HOANGTHAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
